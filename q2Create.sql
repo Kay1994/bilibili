476aa65c449708d47b795287b110b0e0b0cb4dd9
Kay <weijkai520@gmail.com>
2019-03-11 18:01:16 +0100

    DADASD

---
+++ q2Create.sql
@@ -0,0 +1,2 @@
+CREATE MATERIALIZED VIEW Info AS (SELECT cr.StudentRegistrationId, SUM(cr.Grade * c.ECTS)/(SUM(ECTS)*1.0) AS GPA, SUM(ECTS) AS ObtainedECTS FROM CourseRegistrations cr, CourseOffers co, Courses c WHERE cr.CourseOfferId = co.CourseOfferId AND co.courseId = c.courseId AND cr.Grade >= 5 GROUP BY cr.StudentRegistrationId);
+
--- q2Queries.sql
+++ q2Queries.sql
@@ -1,6 +1,6 @@
 SELECT 0;
 SELECT 0;
-SELECT Degrees.DegreeId as degreeid, (cast(SUM(CASE WHEN Students.Gender = 'F' THEN 1 ELSE 0 END) as float) / COUNT(Students.Gender)) as percentage FROM Students INNER JOIN StudentRegistrationsToDegrees on (Students.StudentId = StudentRegistrationsToDegrees.StudentId) INNER JOIN Degrees on (StudentRegistrationsToDegrees.DegreeId = Degrees.DegreeId) GROUP BY Degrees.DegreeId;
+WITH ActiveStudents(StudentRegistrationId) AS (SELECT srd.StudentRegistrationId FROM StudentRegistrationsToDegrees srd INNER JOIN Degrees d ON (srd.DegreeId = d.DegreeId) LEFT JOIN Info i ON (i.StudentRegistrationId = srd.StudentRegistrationId) WHERE i.ObtainedECTS < d.TotalECTS OR i.ObtainedECTS is null), ActiveStudentsByG(DegreeId, Gender, nr) AS (SELECT srd.DegreeId, s.Gender, COUNT(srd.StudentId) FROM StudentRegistrationsToDegrees srd, Students s, ActiveStudents acts WHERE srd.StudentId = s.StudentId AND acts.StudentRegistrationId = srd.StudentRegistrationId GROUP BY srd.DegreeId, s.Gender), TotalActiveStudents(DegreeId, nr) AS (SELECT asbg.DegreeId, SUM(nr) FROM ActiveStudentsByG asbg GROUP BY asbg.DegreeId) SELECT tas.DegreeId, asbg.nr/(tas.nr*1.0) AS percentage FROM ActiveStudentsByG asbg,TotalActiveStudents tas WHERE asbg.Gender = 'F' AND asbg.DegreeId = tas.DegreeId ORDER BY tas.DegreeId;
 SELECT 0;
 SELECT 0;
 SELECT 0;
