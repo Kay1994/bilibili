ae19be02cbc72fdd37c58230319f9d8026dc7a26
Kay <weijkai520@gmail.com>
2019-03-12 21:42:13 +0100

    dasdasd

--- q2Create.sql
+++ q2Create.sql
@@ -1,3 +1,3 @@
-CREATE MATERIALIZED VIEW Info (StudentRegistrationId,totalects) AS (SELECT CourseRegistrations.StudentRegistrationId, SUM(ECTS) AS totalects FROM CourseRegistrations, CourseOffers , Courses  WHERE CourseRegistrations.CourseOfferId = CourseOffers.CourseOfferId AND CourseOffers.courseId = Courses.courseId AND CourseRegistrations.Grade >= 5 GROUP BY CourseRegistrations.StudentRegistrationId);
+CREATE MATERIALIZED VIEW Info (StudentRegistrationId,gpa,totalects) AS (SELECT CourseRegistrations.StudentRegistrationId,SUM(CourseRegistrations.Grade * Courses.ECTS)/(SUM(Courses.ECTS)*1.0) AS gpa, SUM(ECTS) AS totalects FROM CourseRegistrations, CourseOffers , Courses  WHERE CourseRegistrations.CourseOfferId = CourseOffers.CourseOfferId AND CourseOffers.courseId = Courses.courseId AND CourseRegistrations.Grade >= 5 GROUP BY CourseRegistrations.StudentRegistrationId);
 
 
